306c4a5aa47d550b90e19379bb7eb548a30a8a38
alviasi <asifloulou@yahoo.com>
2026-05-01 18:32:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function submitData() {
   alert("Data submitted!");
 }
+function submitData() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  fetch("YOUR_WEB_APP_URL", {
+    method: "POST",
+    body: new URLSearchParams({
+      username: username,
+      password: password
+    })
+  })
+  .then(res => res.text())
+  .then(data => {
+    if(data === "success"){
+      alert("Login success");
+    } else {
+      alert("Wrong username/password");
+    }
+  });
+}
