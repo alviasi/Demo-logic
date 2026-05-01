ea3ff12a4b6e75bd1cb9598f36c86bad6eaded14
alviasi <asifloulou@yahoo.com>
2026-05-01 18:28:31 +0200

    Implement user authentication in doPost function
    Add doPost function to handle user authentication

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function submitData() {
   alert("Data submitted!");
 }
+function doPost(e) {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("users");
+  var data = sheet.getDataRange().getValues();
+
+  var username = e.parameter.username;
+  var password = e.parameter.password;
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i][0] === username && data[i][1] === password) {
+      return ContentService.createTextOutput("success");
+    }
+  }
+
+  return ContentService.createTextOutput("fail");
+}
